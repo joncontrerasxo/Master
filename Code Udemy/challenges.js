5b7928f0d367e0fffe9ac08a5311e42bf41f78a4
joncontrerasxo <110271087+joncontrerasxo@users.noreply.github.com>
2022-08-01 13:12:12 -0700

    Add files via upload

---
+++ Code Udemy/challenges.js
@@ -0,0 +1,61 @@
+/*
+//Coding Challenge #1
+
+const massJohn = 92;
+const massMark = 45;
+const heightJohn = 1.95;
+const heightMark = 1.69;
+let bmiMark;
+let bmiJohn;
+let markHigherBMI;
+
+//BMI formula 1
+
+bmiJohn = Math.round(massJohn / heightJohn ** 2);
+
+console.log("John's BMI is: " + bmiJohn);
+
+bmiMark = Math.round(massMark / heightMark ** 2);
+
+console.log("Mark's BMI is: " + bmiMark);
+
+
+//BMI formula 2
+
+bmiJohn = Math.round(massJohn / (heightJohn ** 2));
+
+console.log("John's BMI is: " + bmiJohn);
+
+bmiMark = Math.round(massMark / (heightMark ** 2));
+
+console.log("Mark's BMI is: " + bmiMark);
+
+markHigherBMI = bmiMark > bmiJohn;
+
+console.log("Is Mark's BMI higher than John's ? " + markHigherBMI);
+
+
+//Coding Challenge #2
+
+if (markHigherBMI == true) {
+    console.log(`Mark's BMI: (${bmiMark}) is higher than John's (${bmiJohn}!!)`);
+} else {
+    console.log(`John's BMI: (${bmiJohn}) is higher than Mark's (${bmiMark}!!)`);
+}
+
+
+const averageDolphin = (96 + 108 + 89) / 3
+const averageKoala = (88 + 91 + 110) / 3
+
+console.log(`Dolphin score:${averageDolphin}`);
+console.log(`Koala score:${averageKoala}`);
+
+if (averageDolphin > averageKoala && averageDolphin >= 100) {
+    console.log(`The winners are the dolphins!!!`);
+} else if (averageKoala >= 100 && averageKoala > averageDolphin) {
+    console.log(`The winners are the koalas!!!`);
+} else if (averageDolphin === averageKoala && averageDolphin >= 100 && averageKoala >= 100) {
+    console.log(`The game is a DRAW!!!`);
+} else console.log(`There are no winners!!!`);
+
+*/
