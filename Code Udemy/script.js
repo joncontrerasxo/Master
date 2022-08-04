27869fd26e3fb0e491fd7885109a4c123972c4cb
joncontrerasxo <110271087+joncontrerasxo@users.noreply.github.com>
2022-08-04 12:04:00 -0700

    Add files via upload

--- Code Udemy/script.js
+++ Code Udemy/script.js
@@ -290,7 +290,7 @@ console.log(drink);
 
 console.log(`I love to drink ${age>=18 ? `Beer!`:`Water!!`}`);
 
-*/
+
 
 //Strict mode test
 
@@ -299,3 +299,81 @@ const passTest = true;
 
 if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log(`You can driveeeee!`);
+
+
+//Functions
+
+function logger() {
+    console.log(`My name is Jonas`);
+}
+//Calling the function
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(3, 10);
+console.log(appleOrangeJuice);
+
+
+
+//Function declaration
+
+function calAge1(birthYear) {
+    return 2037 - birthYear
+}
+
+const age1 = calAge1(1991);
+console.log(age1);
+
+//Function expression
+
+const calcAge2 = function(birthYear) {
+    return 2037 - birthYear
+}
+const age2 = calcAge2(1991);
+console.log(age1, age2);
+
+
+
+//Arrow function
+
+const calcAge3 = birthYear => 2037 - birthYear;
+
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear
+    const retirement = 65 - age;
+    //return retirement;
+    return `${firstName} retires in: ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, `Jonas`));
+console.log(yearsUntilRetirement(1980, `Bob`));
+
+
+
+//Functions inside functions
+
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2, 3));
+
+*/
